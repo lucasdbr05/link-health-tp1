914a1136424927719fae779146016cbca5d3c8ea
Lucas Lima <lucasgabrieljake@gmail.com>
2024-07-04 17:47:18 -0300

    feat: create Produtos class and its methods and attributes

---
+++ linkHealth/src/Produto.java
@@ -0,0 +1,47 @@
+public class Produto {
+       private String nome;
+       private Integer id;
+       private Double precoDeCusto;
+       private boolean exigeReceita;
+
+    public Produto(String nome, Integer id, Double precoDeCusto, boolean exigeReceita) {
+        this.nome = nome;
+        this.id = id;
+        this.precoDeCusto = precoDeCusto;
+        this.exigeReceita = exigeReceita;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getPrecoDeCusto() {
+        return precoDeCusto;
+    }
+
+    public void setPrecoDeCusto(Double precoDeCusto) {
+        this.precoDeCusto = precoDeCusto;
+    }
+
+    public boolean isExigeReceita() {
+        return exigeReceita;
+    }
+
+    public void setExigeReceita(boolean exigeReceita) {
+        this.exigeReceita = exigeReceita;
+    }
+       
+    
+}
