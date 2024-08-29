be7567bbcfcf5ce049fadf6f8281595745585d36
Lucas Lima <lucasgabrieljake@gmail.com>
2024-08-29 19:06:08 -0300

    feat: git ingore changes

--- linkHealth/src/classes/ENV.java
+++ linkHealth/src/classes/ENV.java
@@ -2,7 +2,5 @@ package classes;
 
 public class ENV {
     public static String databasePath = "C:\\Users\\Notebook\\Documents\\NetBeansProjects\\link-health-tp1\\linkHealth\\src\\classes\\database\\data";
-    public static void main(String[] args) {
-        
-    }
+
 }
