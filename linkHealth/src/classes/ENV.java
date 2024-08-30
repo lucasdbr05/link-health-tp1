6c32718fe47757902c5f274db3debaed382ea721
Lucas Lima <lucasgabrieljake@gmail.com>
2024-08-30 16:24:23 -0300

    ignore

--- linkHealth/src/classes/ENV.java
+++ linkHealth/src/classes/ENV.java
@@ -2,4 +2,5 @@ package classes;
 
 public class ENV {
     public static String databasePath = "C:\\Users\\Notebook\\Documents\\NetBeansProjects\\link-health-tp1\\linkHealth\\src\\classes\\database\\data";
+    public static String databasePath2 = "C:\\Users\\Notebook\\Documents\\NetBeansProjects\\link-health-tp1\\linkHealth\\src\\classes\\database\\data";
 }
